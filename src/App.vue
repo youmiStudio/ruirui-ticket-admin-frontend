56ff3232fd948ddd565fd3301a4e602f15382fa1
LiYuze <228487099@qq.com>
2022-09-28 11:18:28 +0800

    update:defalut.scss

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,3 @@
     hi
   </el-config-provider>
 </template>
-
-<style>
-#app {
-  text-align: center;
-  color: var(--ep-text-color-primary);
-}
-
-.element-plus-logo {
-  width: 50%;
-}
-</style>
